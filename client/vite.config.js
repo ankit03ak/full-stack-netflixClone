03c0ff8f40d5807e5817b4c04978383ed41b326f
Ankit Kumar <vickyak1339@gmail.com>
2025-02-18 02:27:01 +0530

    fixed vide.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
     proxy: {
       '/api': 'https://netflix-api1-4syc.onrender.com', // Proxy /api to your backend
     },
+  },
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
   },
 });
